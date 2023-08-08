2406aefae80719afcb3aa99d3df341d001ecc3bd
Himanth24 <himanthgodari@gmail.com>
2023-08-08 18:22:32 +0530

    created the container with inner divs using grid

---
+++ main.js
@@ -0,0 +1,11 @@
+let numberOfSquares = prompt('enter the number');
+const container = document.querySelector('.container');
+container.style.gridTemplateColumns = `repeat(${numberOfSquares},1fr)`;
+container.style.gridTemplateRows = `repeat(${numberOfSquares},1fr)`;
+let amount = numberOfSquares * numberOfSquares;
+for(let i=0;i<amount;i++){
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'aqua';
+    container.insertAdjacentElement('beforeend',square);
+    
+}
