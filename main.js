67fa7b135b80eb35b8d2a4d96bc58bc7d694a885
Himanth24 <himanthgodari@gmail.com>
2023-08-09 18:22:14 +0530

    added functionality to buttons

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ container.style.gridTemplateRows = `repeat(${numberOfSquares},1fr)`;
 let amount = numberOfSquares * numberOfSquares;
 for(let i=0;i<amount;i++){
     let square = document.createElement('div');
+    square.className = 'squares';
     square.style.backgroundColor = 'white';
     container.insertAdjacentElement('beforeend',square);
     square.addEventListener('mousedown',()=>{
@@ -12,5 +13,45 @@ for(let i=0;i<amount;i++){
     })
 }
 
+let squares = document.querySelectorAll('.squares')
+
+function reset(){
+    squares.forEach(square => {
+        square.style.backgroundColor='white';
+    });
+}
+
+function eraser(){
+    squares.forEach(square=>{
+        square.addEventListener('mousedown',()=>{
+            square.style.backgroundColor='white';
+        })
+    })
+}
+
+function black(){
+    squares.forEach(square=>{
+        square.addEventListener('mousedown',()=>{
+            square.style.backgroundColor='black';
+        })
+    })
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 
+function random(){
+    squares.forEach(square=>{
+        square.addEventListener('mousedown',()=>{
+            let randomColor = getRandomColor();
+            square.style.backgroundColor=randomColor;
+        })
+    })
+}
